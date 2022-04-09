ff038f2cc5e9becfe5dc5c313a6730073b65b9c3
svgaming234 <100072489+svgaming234@users.noreply.github.com>
2022-04-09 21:38:26 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # clicknspam
-clicknspam is a program that has an autoclicker and text spammer in one program. It is written in Python.
+clicknspam is a program that has an autoclicker and text spammer in one program. It is written in Python. <br>
+Dependencies: <br>
+Python 3, of course (Python 3.9 is not supported) <br>
+PySimpleGUI <br>
+PyAutoGUI <br>
---
+++ clicknspam.py
@@ -0,0 +1,71 @@
+import PySimpleGUI as sg
+import time
+import pyautogui
+
+def clicker():
+    layout = [  [sg.Text('Enter the time between clicks(in seconds):'), sg.InputText('', size=(10,1), key='in1')],
+                [sg.Text('Enter the amount of clicks you want the program to click(leave blank to click until the program is closed): '), sg.InputText('', size=(10,1), key='in2')],
+                [sg.Button('Start Clicking!'), sg.Button('Close')]] 
+
+    window = sg.Window('Autoclicker', layout).Finalize()
+    
+    while True:
+        event, values = window.read()
+        if event == 'Close': 
+            break
+        if event == 'Start Clicking!': 
+            a = int(values['in1'])
+            b = values['in2']
+            c = 0
+            while True:
+                time.sleep(a)
+                pyautogui.click()
+                # the part below is very messy,
+                # but it was the only way i could make it click forever if left blank
+                c = c + 1
+                d = str(c)
+                if d == b:
+                    break
+            
+    window.close()
+
+def txtspam():
+    layout = [  [sg.Text('Enter name of the file with the script you want to spam (not including extension, extension must be .txt):'), sg.InputText('', size=(10,1), key='in1')],
+                [sg.Text('Delay between messages (in seconds, must be decimal number (ex. 0.4, 1.0, etc.)):'), sg.InputText('', size=(10,1), key='in2')],
+                [sg.Text('Time to select the chatbox you want the bot to spam in before the bot start spamming (in seconds, must be a full number (ex. 3, 5, etc.)):'), sg.InputText('', size=(10,1), key='in3')],
+                [sg.Button('Start Spamming!'), sg.Button('Close')]] 
+
+    window = sg.Window('Text Spammer', layout).Finalize()
+    
+    while True:
+        event, values = window.read()
+        if event == 'Close': 
+            break
+        if event == 'Start Spamming!':
+            fname = values['in1']
+            dtime = float(values['in2'])
+            wtime = int(values['in3'])
+            time.sleep(wtime)
+            f = open(fname + ".txt", "r")
+            for word in f:
+                pyautogui.typewrite(word)
+                pyautogui.press("enter")
+                time.sleep(dtime)
+
+    window.close()
+
+sg.theme('Black')
+
+layout = [[sg.Text("Pick what you want to use:")], [sg.Button("Autoclicker")], [sg.Button("Text Spammer")], [sg.Button("Close")]]
+
+window = sg.Window("ClickNSpam", layout)
+
+while True:
+    event, values = window.read()
+    if event == "Close" or event == sg.WIN_CLOSED:
+        break
+    if event == "Autoclicker":
+        clicker()
+    if event == "Text Spammer":
+        txtspam()
+window.close()
